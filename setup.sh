1ccf65063ec467242e962ec7cbbdaa9a233545f9
ivandudin29 <ivan.dudin29@gmail.com>
2026-02-07 22:38:39 +0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Очищаем кэш pip и виртуальное окружение перед установкой
-rm -rf .venv
-pip cache purge || true
-python -m venv .venv
-source .venv/bin/activate
-pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt
